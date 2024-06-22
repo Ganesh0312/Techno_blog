b241372742b602947fc8452e340da571ae4a3243
Ganesh0312 <bobadeganeshd@gmail.com>
2024-06-23 02:02:55 +0530

    seacrh and filter is completed

--- blog_client/src/App.jsx
+++ blog_client/src/App.jsx
@@ -13,6 +13,7 @@ import CreatePost from "./pages/CreatePost";
 import UpdatePost from "./pages/UpdatePost";
 import PostPage from "./pages/PostPage";
 import ScrollToTop from "./components/ScrollToTop";
+import Search from "./pages/Search";
 
 const App = () => {
   return (
@@ -33,6 +34,7 @@ const App = () => {
           </Route>
           <Route path="/signin" element={<SignIn />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/search" element={<Search />} />
           <Route path="/projects" element={<Projects />} />
         </Routes>
         <FooterCom />
