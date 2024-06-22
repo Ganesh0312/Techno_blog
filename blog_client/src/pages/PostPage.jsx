0c43cd870c2bc3b4f48f48981723be426a6c9cbc
Ganesh0312 <bobadeganeshd@gmail.com>
2024-06-23 01:14:06 +0530

    recent arlticale section added

--- blog_client/src/pages/PostPage.jsx
+++ blog_client/src/pages/PostPage.jsx
@@ -2,6 +2,7 @@ import { Button, Spinner } from "flowbite-react";
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import CommentSection from "../components/CommentSection";
+import PostCard from "../components/PostCard";
 // import CallToAction from "../components/CallToAction";
 // import CommentSection from "../components/CommentSection";
 // import PostCard from "../components/PostCard";
@@ -94,8 +95,8 @@ export default function PostPage() {
       <div className="flex flex-col justify-center items-center mb-5">
         <h1 className="text-xl mt-5">Recent articles</h1>
         <div className="flex flex-wrap gap-5 mt-5 justify-center">
-          {/* {recentPosts && */}
-          {/* recentPosts.map((post) => <PostCard key={post._id} post={post} />)} */}
+          {recentPosts &&
+            recentPosts.map((post) => <PostCard key={post._id} post={post} />)}
         </div>
       </div>
     </main>
