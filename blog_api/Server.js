fd22057326c63b0097e39d3962895019f140675b
Ganesh0312 <bobadeganeshd@gmail.com>
2024-06-23 10:14:43 +0530

    new comiit

--- blog_api/Server.js
+++ blog_api/Server.js
@@ -32,10 +32,10 @@ app.use("/api/post", postRoute);
 app.use("/api/comment", commentRoutes);
 
 const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, "/client/dist")));
+app.use(express.static(path.join(__dirname, "/blog_client/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "blog_client", "dist", "index.html"));
 });
 
 app.use((err, req, res, next) => {
